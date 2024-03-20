59811fe76349de946c138252a70968433e86e13f
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-20 12:12:27 +0400

    Добавление моделей

--- Questioner/admin.py
+++ Questioner/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from Questioner.models import Survey, Answer, Question, Link
+
+
+@admin.register(Survey)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Question)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Link)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- Questioner/models.py
+++ Questioner/models.py
@@ -3,23 +3,49 @@ from django.db import models
 
 class Survey(models.Model):
     """Модель опроса"""
-    title = models.CharField(max_length=255, name="Название опроса")
+    title = models.CharField(max_length=255, verbose_name="Название опроса")
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
 
 
 class Question(models.Model):
     """Модель вопроса"""
-    text = models.TextField(max_length=1000, name="Текст вопроса")
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name="Опрос", related_name="questions")
+    text = models.TextField(max_length=1000, verbose_name="Текст вопроса")
+    survey = models.ManyToManyField(Survey, verbose_name="Опрос", related_name="questions")
+
+    def __str__(self):
+        return f"{self.id}: {self.text}"
 
 
 class Answer(models.Model):
     """Модель ответа"""
     text = models.CharField(max_length=255, verbose_name="Текст ответа")
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос", related_name="answers")
+    question = models.ManyToManyField(Question, verbose_name="Вопрос", related_name="answers")
+
+    def __str__(self):
+        return f"{self.id}: {self.text}"
 
 
 class Link(models.Model):
     """Связь последовательности вопросов"""
-    from_question = models.ForeignKey(Question, related_name='from_question', on_delete=models.CASCADE)
-    to_question = models.ForeignKey(Question, related_name='to_question', on_delete=models.CASCADE)
-    answer = models.ForeignKey(Answer, blank=True, null=True, on_delete=models.CASCADE)
+    from_question = models.ForeignKey(
+        Question,
+        related_name='from_question',
+        on_delete=models.CASCADE,
+        verbose_name="От какого вопроса"
+    )
+    to_question = models.ForeignKey(
+        Question,
+        related_name='to_question',
+        on_delete=models.CASCADE,
+        verbose_name="К какому вопросу"
+    )
+    survey = models.ForeignKey(
+        Survey,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="links",
+        verbose_name="В каком опросе"
+    )
